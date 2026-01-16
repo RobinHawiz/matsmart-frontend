6c04d1be05d0a8c454ab0e456d470756bd9c0f77
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-01-16 17:06:09 +0100

    fix(header): align active link underline with hover state

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,15 +10,15 @@ import { RouterLink } from "vue-router";
       <ul class="flex gap-4">
         <li>
           <RouterLink
-            activeClass="after:opacity-100"
-            class="after:bg-primary-dark-3 text-primary-dark-2 relative after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-[99%] after:opacity-0 after:transition-all after:duration-200 hover:after:translate-y-1 hover:after:opacity-100"
+            activeClass="after:opacity-100 after:-bottom-1"
+            class="after:bg-primary-dark-3 text-primary-dark-2 relative after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-[99%] after:opacity-0 after:transition-all after:duration-200 hover:after:translate-y-0.5 hover:after:opacity-100"
             to="/lager"
             >Lager</RouterLink
           >
         </li>
         <li>
           <RouterLink
-            activeClass="after:opacity-100"
+            activeClass="after:opacity-100 after:-bottom-1"
             class="after:bg-primary-dark-3 text-primary-dark-2 relative after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-full after:opacity-0 after:transition-all after:duration-200 hover:after:translate-y-0.5 hover:after:opacity-100"
             to="/artiklar"
             >Artiklar</RouterLink
@@ -26,7 +26,7 @@ import { RouterLink } from "vue-router";
         </li>
         <li class="ml-auto">
           <RouterLink
-            activeClass="after:opacity-100"
+            activeClass="after:opacity-100 after:-bottom-1"
             class="after:bg-primary-dark-3 text-primary-dark-2 relative after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-full after:opacity-0 after:transition-all after:duration-200 hover:after:translate-y-0.5 hover:after:opacity-100"
             to="/logout"
             >Logga ut</RouterLink

eb6a61b646e273cda455a13899945ac2c2098aa6
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-01-18 17:02:49 +0100

    style(artiklar): add hover shadow to "Ny artikel" button

--- src/views/ArtiklarView.vue
+++ src/views/ArtiklarView.vue
@@ -14,7 +14,7 @@ const title = "Artiklar";
   <RouterLink
     :to="{ name: 'NyArtikel' }"
     aria-label="Skapa en ny artikel"
-    class="bg-primary-brand-1 text-primary-light-1 leading-body hover:bg-primary-brand-2 fixed right-5.75 bottom-4.5 flex cursor-pointer items-center gap-2 rounded-full px-3 py-1.5 transition-colors duration-200 ease-in-out md:right-11.5 md:bottom-9.25"
+    class="bg-primary-brand-1 text-primary-light-1 hover:shadow-elevation-low leading-body hover:bg-primary-brand-2 fixed right-5.75 bottom-4.5 flex cursor-pointer items-center gap-2 rounded-full px-3 py-1.5 transition-all duration-200 ease-in-out md:right-11.5 md:bottom-9.25"
   >
     <img :src="plus" aria-hidden="true" class="h-3.25 w-3.25" />
     Ny artikel
